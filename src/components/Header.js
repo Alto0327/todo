39c95f2729cacf69442fb422ba6e839bc05bc745
Alto0327 <aj.silvaf2003@gmail.com>
2024-02-12 23:55:42 -0500

    Create a Creating Task Modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,45 @@
-function Header() {
-    return (  
-        <div>
-            <ul className="ContainerNavbar">
+import React, { useState } from 'react';
+import './Modal.css';
+
+const Modal = ({ showModal, closeModal }) => {
+  return (
+    <div className={`modal ${showModal ? 'show' : 'hide'}`} onClick={closeModal}>
+      <div className="modal-dialog" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-content">
+          <p>This is your modal content.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Header = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div>
+      <ul className="ContainerNavbar">
                 <li>
                     <h1 className="Title-NavBar">ToDo</h1> 
                 </li>
-                <li>
-                    <button className="Button-NavBar">Create New Task</button>  
+                <li>  
+                    <button className="Button-NavBar" onClick={openModal}>Create New Task</button>
+                    {showModal && <Modal showModal={showModal} closeModal={closeModal} />}
                 </li>
                 <li>
                     <button>Change theme</button>
                 </li>
-
             </ul>
-            
-        </div>
-    );
-
+    </div>
+  );
 };
 
-export default Header
+export default Header;
